315ed36d19072e0a2e82a1c7dfc76f7891e46e12
Jesus Vazquez <57465581+Yizuz02@users.noreply.github.com>
2023-11-13 09:59:22 -0600

    Add files via upload

---
+++ Lista4_Ejercicio5/mainPila.c
@@ -0,0 +1,138 @@
+#include<stdio.h>
+#include "pila.h"
+#include<stdlib.h>
+
+void menu(PILA, PILA);
+void mostrarOpciones();
+void manejaMsg(int);
+PILA datosEntrada();
+void mostrarPila(PILA);
+void liberarMem(PILA);
+
+void main(){
+	PILA P, copyP;
+	P=datosEntrada();
+	menu(P, copyP);
+	mostrarPila(P);
+	liberarMem(P);
+}
+
+void menu(PILA P, PILA copyP){
+    int op;
+    do{
+        mostrarOpciones();
+        printf("Ingrese el numero de la accion que desea realizar: ");
+        scanf("%d",&op);
+        printf("\n");
+        switch (op)
+        {
+        case 1:
+            printf("La pila tiene %d elementos\n",numElem(P));
+            break;
+        
+        case 2:
+			printf("El elemento al fondo de la pila es %d\n",elemFondo(P));
+            break;
+        case 3:
+            copyP = copiarFila(P);
+            break;
+        case 4:
+            liberarMem(P);
+            break;
+
+        default:
+            printf("¡Opcion no valida! Ingrese una opcion valida\n");
+            break;
+        }
+    }while(op!=4);
+}
+
+void mostrarOpciones(){
+    printf("\n\t\tMenu Pilas\n\n");
+    printf("1.- Numero de elementos\n");
+    printf("2.- Elemento del fondo\n");
+    printf("3.- Copiar Pila\n");
+    printf("4.- Salir\n\n");
+}
+
+PILA datosEntrada(){
+	PILA P = crearPila();
+	int n, op=0;
+	printf("Ingrese los datos que desea agregar a la pila:\n");
+	while(1){
+		scanf("%d",&n);
+		if (n=='\n')
+			break;
+		apilar(P, n);
+	}
+	return P;
+}
+
+PILA copiarFila(PILA P){
+	PILA temp = crearPila();
+	PILA C = crearPila();
+	int n, sum=0;
+	while(!es_vaciaPila(P)){
+		n = desapilar(P);
+		sum++;
+		apilar(temp, n);
+	}
+	printf("\n");
+	while(!es_vaciaPila(temp)){
+		n= desapilar(temp);
+		apilar(P, n);
+		apilar(C, n);
+	}
+	return C;
+}
+
+int elemFondo(PILA P){
+	PILA temp = crearPila();
+	int n, sum=0;
+	while(!es_vaciaPila(P)){
+		n = desapilar(P);
+		apilar(temp, n);
+	}
+	printf("\n");
+	while(!es_vaciaPila(temp))
+		apilar(P, desapilar(temp));
+	return n;
+}
+
+int elemFondo(PILA P){
+	PILA temp = crearPila();
+	int n, sum=0;
+	while(!es_vaciaPila(P)){
+		n = desapilar(P);
+		apilar(temp, n);
+	}
+	printf("\n");
+	while(!es_vaciaPila(temp))
+		apilar(P, desapilar(temp));
+	return n;
+}
+
+void mostrarPila(PILA P){
+	PILA temp = crearPila();
+	int n;
+	while(!es_vaciaPila(P)){
+		n = desapilar(P);
+		printf("%c",n);
+		apilar(temp, n);
+	}
+	printf("\n");
+	while(!es_vaciaPila(temp))
+		apilar(P, desapilar(temp));
+}
+
+void manejaMsg(int msg){
+	char * mensajes[] = {"No hay memoria disponible . . .","Se ha liberado la memoria . . .  ","La Pila esta llena . . .","La Pila esta vacia . . ."};
+	printf("%s", mensajes[msg] );
+}
+
+void liberarMem(PILA P){
+	free(P);
+	manejaMsg(1);
+}
+
+
